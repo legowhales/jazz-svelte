cfcaefb674c6a1adece156d9feb3d3ef5c795939
Jérémy Le Mardelé <jeremy@otarie.co>
2024-11-20 21:22:54 +0100

    fix: clerk auth issue if no user

--- src/lib/jazz/auth/clerk.svelte.ts
+++ src/lib/jazz/auth/clerk.svelte.ts
@@ -1,20 +1,31 @@
 import { BrowserClerkAuth, type MinimalClerkClient } from 'jazz-browser-auth-clerk';
 import { box, type MaybeBoxOrGetter } from 'svelte-toolbelt';
+import { untrack } from 'svelte';
 
-export function useJazzClerkAuth(client: MaybeBoxOrGetter<MinimalClerkClient>) {
+export function useJazzClerkAuth(
+	client: MaybeBoxOrGetter<
+		MinimalClerkClient & {
+			signOut: () => Promise<unknown>;
+		}
+	>
+) {
 	let state = $state<{ errors: string[] }>({ errors: [] });
 	let _client = box.from(client);
+
 	let auth = $derived.by(() => {
-		if (!_client.current.user) return;
-		if (!_client.current.signOut) return console.log('no signOut function found');
-		return new BrowserClerkAuth(
-			{
-				onError: (error) => {
-					void _client.current.signOut();
-					state = { ...state, errors: [...state.errors, error.toString()] };
-				}
-			},
-			_client.current
+		_client.current.user;
+
+		return untrack(
+			() =>
+				new BrowserClerkAuth(
+					{
+						onError: (error) => {
+							void _client.current.signOut?.();
+							state = { ...state, errors: [...state.errors, error.toString()] };
+						}
+					},
+					_client.current
+				)
 		);
 	});
 
